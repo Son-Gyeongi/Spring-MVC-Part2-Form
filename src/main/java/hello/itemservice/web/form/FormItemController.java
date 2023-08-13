44f728667f8886614618efff083664477ee8dfed
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-13 15:26:22 +0900

    타임리프 - 스프링 통합과 폼 / 체크 박스 - 멀티

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,20 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * FormItemController에서 어떤 호출이든 자동으로 model에 regions가 들어간다.
+     */
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        // 멀티 데이터 넘겨주기
+        // HashMap - 순서 보장 안됨, LinkedHashMap - 순서 보장 됨
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -30,6 +46,15 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        // 멀티 데이터 넘겨주기
+        // HashMap - 순서 보장 안됨, LinkedHashMap - 순서 보장 됨
+//        Map<String, String> regions = new LinkedHashMap<>();
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//        model.addAttribute("regions", regions);
+
         return "form/item";
     }
 
@@ -38,6 +63,15 @@ public class FormItemController {
         // 타임리프가 지원하는 폼 기능을 쓰려면 먼저 model을 하나 넘겨줘야 한다.
         // 빈 아이템이라도 넘겨줘야 한다.
         model.addAttribute("item", new Item());
+
+        // 멀티 데이터 넘겨주기
+        // HashMap - 순서 보장 안됨, LinkedHashMap - 순서 보장 됨
+//        Map<String, String> regions = new LinkedHashMap<>();
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//        model.addAttribute("regions", regions);
+
         return "form/addForm";
     }
 
@@ -45,6 +79,9 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         // open 값이 잘 넘어왔는지 확인
         log.info("item.open = {}", item.getOpen());
+        // regions 값이 잘 들어갔는지 확인
+        log.info("item.regions = {}", item.getRegions());
+        
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -55,6 +92,15 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        // 멀티 데이터 넘겨주기
+        // HashMap - 순서 보장 안됨, LinkedHashMap - 순서 보장 됨
+//        Map<String, String> regions = new LinkedHashMap<>();
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//        model.addAttribute("regions", regions);
+
         return "form/editForm";
     }
 
